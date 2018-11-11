b82e997d5a0e8e237a6fbb075f36bc9081f68ca4
Aleksej Bykov <lebykov@gmail.com>
2018-11-11 19:49:38 +0300

    добавил новую шкалу САТ; обновил тесты

--- src/freiburger_evaluator/scale.py
+++ src/freiburger_evaluator/scale.py
@@ -3,6 +3,8 @@ class Scale(object):
     def factory(scale_name, **kwargs):
         if scale_name == 'frei':
             return FreiburgerScale(**kwargs)
+        elif scale_name == 'cat':
+            return CatScale(**kwargs)
 
 
 class FreiburgerScale(Scale):
@@ -15,7 +17,7 @@ class FreiburgerScale(Scale):
         self.standard_keys = standard_keys
         self.raw_score = 0
         self.standard_score = 0
-        self.check_defaults()
+        self._check_defaults()
 
     def __repr__(self):
         return f'Scale(\n\tname={self.name}, \n\tnumber={self.number}, \n\tyanswers={self.yanswers}, ' \
@@ -31,6 +33,29 @@ class FreiburgerScale(Scale):
     def convert_to_standard(self):
         self.standard_score = self.standard_keys[self.raw_score]
 
-    def check_defaults(self):
+    def _check_defaults(self):
         assert self.raw_score == 0
         assert self.standard_score == 0
+
+
+class CatScale(Scale):
+    def __init__(self, number, name, aanswers, banswers):
+        super().__init__()
+        self.number = number
+        self.name = name,
+        self.aanswers = aanswers
+        self.banswers = banswers
+        self.raw_score = 0
+
+    def __repr__(self):
+        return f'Scale(\n\tname={self.name}, \n\tnumber={self.number}, \n\taanswers={self.aanswers}, ' \
+               f'\n\tbanswers={self.banswers}, \n\traw_score={self.raw_score}'
+
+    def check_answer(self, question_numbers, answer):
+        if answer == 'а' and question_numbers in self.aanswers:
+            self.raw_score += 1
+        if answer == 'б' and question_numbers in self.banswers:
+            self.raw_score += 1
+
+    def _check_defaults(self):
+        assert self.raw_score == 0
--- tests/unit/test_scale.py
+++ tests/unit/test_scale.py
@@ -4,8 +4,9 @@ from freiburger_evaluator import scale
 
 class TestScale:
 
-    def test_check_answer(self):
-        test_scale = scale.Scale(
+    def test_freiburger_check_answer(self):
+        test_scale = scale.Scale.factory(
+            scale_name='frei',
             number='1',
             name='Test',
             yanswers={1, 3},
@@ -25,5 +26,27 @@ class TestScale:
         test_scale.check_answer(4, 'Да')
         assert test_scale.raw_score == 2
         test_scale.check_answer(2, 'Не знаю')
+        assert test_scale.raw_score == 2
+
+    def test_cat_check_answer(self):
+        test_scale = scale.Scale.factory(
+            scale_name='cat',
+            number='1',
+            name='Test',
+            aanswers={1, 3},
+            banswers={2}
+        )
+        assert test_scale.raw_score == 0
+        test_scale.check_answer(1, 'а')
+        assert test_scale.raw_score == 1
+        test_scale.check_answer(2, 'б')
+        assert test_scale.raw_score == 2
+        test_scale.check_answer(3, 'б')
+        assert test_scale.raw_score == 2
+        test_scale.check_answer(2, 'а')
+        assert test_scale.raw_score == 2
+        test_scale.check_answer(4, 'а')
+        assert test_scale.raw_score == 2
+        test_scale.check_answer(2, 'в')
         assert test_scale.raw_score == 2
 
